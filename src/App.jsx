4774a10c3db97a2b8746936d51f44ed13c80e35c
mcfoster007 <mariechantalfoster@gmail.com>
2024-11-14 10:11:34 -0500

    changed the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
 
-import './App.css';
+
+import './index.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
