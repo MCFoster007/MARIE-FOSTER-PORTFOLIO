4689974de6a766c063b71c4fe0ecc794d05b6511
mcfoster007 <mariechantalfoster@gmail.com>
2024-11-14 01:29:51 -0500

    deleted and added

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+   <Router>
+      <Navbar/>
+      <Switch>
+        <Route path='/'exact />
+      </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
+
+
+
+
+
+      {/* <div>
+      <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+       
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div> */}
