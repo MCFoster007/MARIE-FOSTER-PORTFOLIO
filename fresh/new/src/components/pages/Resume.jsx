f70e8373a099b503892abf6a79c83e485a238755
mcfoster007 <mariechantalfoster@gmail.com>
2024-11-15 00:35:42 -0500

    add resume section

--- fresh/new/src/components/pages/Resume.jsx
+++ fresh/new/src/components/pages/Resume.jsx
@@ -3,13 +3,40 @@ export default function Resume() {
     <div>
       <h1>Resume</h1>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+        <ul>
+          <li>
+            <a href="portfolio.html">Portfolio</a>
+          </li>
+          <li><a href="contactform.html">Contact Form</a></li>
+          <li><a href="index.html">Home</a></li>
+        </ul>
+       </p> 
+       <p>
+        <ul>
+    
+          <div className="header">
+            <div className="container">
+                <div className="card">
+                    <div className="card-inner">
+                        <div className="front-face">
+                            <h3>Check out my resume</h3>
+                            <img src="./src/assets/cartoon me.jpeg" alt="a cartoon pic of me"width ="400" height="375"/>
+                            
+                        </div>
+                        <div className="back-face">
+                            <p>Marie-Chantal Foster
+                        <li><a href="https://docs.google.com/document/d/1aWvLZZx3rwvq-lDyTtr10MGJAsddwzkZsmLWp3kuh-8/edit?tab=t.0">My Resume</a></li>
+                        <li><a href="https://www.linkedin.com/in/mariechantalfoster">My 
+                             LinkedIn</a></li>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        </ul>
+       </p> 
     </div>
   );
 }
