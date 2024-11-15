0fd57365e18ba9f67dcb653f703a652468bab221
mcfoster007 <mariechantalfoster@gmail.com>
2024-11-15 00:32:20 -0500

    added css and about me

--- fresh/new/src/components/pages/About.jsx
+++ fresh/new/src/components/pages/About.jsx
@@ -3,17 +3,43 @@ export default function About() {
     <div>
       <h1>About Me</h1>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
-      </p>
+      <img
+        src="./public/assets/images/fish.jpg"
+        alt="fish" width="200"
+      height="200"  className="align-left medium" />
+
+      <b><i>I am</i></b> a bit late in the game of coding, but it is never to
+      late to teach an old dog new tricks! ha! I touched upon HTML over 20 years
+      ago, trying to build a website for my mom and I. We wanted to sell items
+      we made. I was also finishing college and got my degree in Exercise
+      Science and Nutrition. Guess you could say, life got in the way and never
+      finish it. Since then I did personal training besides working out, and
+      entrered the world of sales. My interest in world of developing did not
+      appear again about 3 years ago when I got into Software Sales. The
+      developers of the application I was selling, showed me the world of
+      developing and what you can do with it. I was eager to learn more but
+      unfortunately the time was not right until now. I was presented the
+      opportunity and just ran with it. The world of developing gives me the
+      opportunity to be creative, think logically and problem solve. Being able
+      to use technology and your noodle to present creations that folks go,
+      Wowza!, or solve the problems they did not see, will be such a
+      satifaction. There is a reason I am here, and I am going to show it! 
+
+
+
+      
+      <img
+        src="./public/assets/images/crab.jpg"
+        alt="crab"  width="100"  height="100" 
+        className="align-right medium"/>
+             Cheers to coding,
+      <b><i>Marie-Chantal Foster</i></b>
+    </p>
     </div>
+
+//      <div className="footer">
+//      <p>Created with Love; by Mighty Marie</p>
+//      <p>© 2024 All Rights Reserved.</p>
+//  </div>
   );
 }
